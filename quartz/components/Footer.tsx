5c3a494ba5e5cebd6231041ed4564ae2bb7ea4a9
ashu <psashuthosh@gmail.com>
2024-08-31 14:21:12 +0200

    Quartz sync: Aug 31, 2024, 2:21 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,10 +13,12 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        {/* Commented out footer content
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
+        */} 
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
